huuphi9x/cd0384-java-application-deployment-projectstarter

Update logic to complete the exercise

starter/catpoint-parent/image-service/src/main/java/module-info.java
@@ -1,0 +1,10 @@
+module com.udacity.catpoint.imagemodule {
+  requires java.desktop;
+  requires org.slf4j;
+  requires software.amazon.awssdk.auth;
+  requires software.amazon.awssdk.core;
+  requires software.amazon.awssdk.regions;
+  requires software.amazon.awssdk.services.rekognition;
+
+  exports com.udacity.catpoint.image.service;
+}

starter/catpoint-parent/security-service/src/main/java/com/udacity/catpoint/security/application/CatpointGui.java
@@ -1,0 +1,43 @@
+package com.udacity.catpoint.security.application;
+
+import com.udacity.catpoint.image.service.FakeImageService;
+import com.udacity.catpoint.image.service.ImageService;
+import com.udacity.catpoint.security.data.PretendDatabaseSecurityRepositoryImpl;
+import com.udacity.catpoint.security.data.SecurityRepository;
+import com.udacity.catpoint.security.service.SecurityService;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+
+/**
+ * This is the primary JFrame for the application that contains all the top-level JPanels.
+ *
+ * We're not using any dependency injection framework, so this class also handles constructing
+ * all our dependencies and providing them to other classes as necessary.
+ */
+public class CatpointGui extends JFrame {
+    private SecurityRepository securityRepository = new PretendDatabaseSecurityRepositoryImpl();
+    private ImageService imageService = new FakeImageService();
+    private SecurityService securityService = new SecurityService(securityRepository, imageService);
+    private DisplayPanel displayPanel = new DisplayPanel(securityService);
+    private SensorPanel sensorPanel = new SensorPanel(securityService);
+    private ControlPanel controlPanel = new ControlPanel(securityService, sensorPanel);
+    private ImagePanel imagePanel = new ImagePanel(securityService);
+
+    public CatpointGui() {
+        setLocation(100, 100);
+        setSize(600, 850);
+        setTitle("Very Secure App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new MigLayout());
+        mainPanel.add(displayPanel, "wrap");
+        mainPanel.add(imagePanel, "wrap");
+        mainPanel.add(controlPanel, "wrap");
+        mainPanel.add(sensorPanel);
+
+        getContentPane().add(mainPanel);
+
+    }
+}

starter/catpoint-parent/security-service/src/main/java/module-info.java
@@ -1,0 +1,10 @@
+module com.udacity.catpoint.securitymodule {
+  requires com.udacity.catpoint.imagemodule;
+  opens com.udacity.catpoint.security.data to com.google.gson;
+
+  requires com.google.common;
+  requires com.google.gson;
+  requires java.desktop;
+  requires java.prefs;
+  requires miglayout;
+}

huuphi9x/udacity-java-programing-pj3

starter/catpoint-parent/image-service/src/main/java/module-info.java
@@ -1,0 +1,10 @@
+module com.udacity.catpoint.image {
+  requires java.desktop;
+  requires org.slf4j;
+  requires software.amazon.awssdk.auth;
+  requires software.amazon.awssdk.core;
+  requires software.amazon.awssdk.regions;
+  requires software.amazon.awssdk.services.rekognition;
+
+  exports com.udacity.catpoint.image.service;
+}

starter/catpoint-parent/security-service/src/main/java/com/udacity/catpoint/security/application/CatpointGui.java
@@ -1,0 +1,43 @@
+package com.udacity.catpoint.security.application;
+
+import com.udacity.catpoint.image.service.FakeImageService;
+import com.udacity.catpoint.image.service.ImageService;
+import com.udacity.catpoint.security.data.SecurityRepository;
+import com.udacity.catpoint.security.data.SecurityRepositoryImpl;
+import com.udacity.catpoint.security.service.SecurityService;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+
+/**
+ * This is the primary JFrame for the application that contains all the top-level JPanels.
+ *
+ * We're not using any dependency injection framework, so this class also handles constructing
+ * all our dependencies and providing them to other classes as necessary.
+ */
+public class CatpointGui extends JFrame {
+    private SecurityRepository securityRepository = new SecurityRepositoryImpl();
+    private ImageService imageService = new FakeImageService();
+    private SecurityService securityService = new SecurityService(securityRepository, imageService);
+    private DisplayPanel displayPanel = new DisplayPanel(securityService);
+    private SensorPanel sensorPanel = new SensorPanel(securityService);
+    private ControlPanel controlPanel = new ControlPanel(securityService, sensorPanel);
+    private ImagePanel imagePanel = new ImagePanel(securityService);
+
+    public CatpointGui() {
+        setLocation(100, 100);
+        setSize(600, 850);
+        setTitle("Very Secure App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new MigLayout());
+        mainPanel.add(displayPanel, "wrap");
+        mainPanel.add(imagePanel, "wrap");
+        mainPanel.add(controlPanel, "wrap");
+        mainPanel.add(sensorPanel);
+
+        getContentPane().add(mainPanel);
+
+    }
+}

starter/catpoint-parent/security-service/src/main/java/module-info.java
@@ -1,0 +1,10 @@
+module com.udacity.catpoint.security {
+  requires com.udacity.catpoint.image;
+  opens com.udacity.catpoint.security.data to com.google.gson;
+
+  requires com.google.common;
+  requires com.google.gson;
+  requires java.desktop;
+  requires java.prefs;
+  requires miglayout;
+}